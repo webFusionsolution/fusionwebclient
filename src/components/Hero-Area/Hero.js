074220360cd2878c7dfbf99c86648df933a89a57
DESKTOP-72D0N0M\Dell <webfusionsolution@gmail.com>
2023-03-18 14:04:06 +0530

    fusion web client updates

--- src/components/Hero-Area/Hero.js
+++ src/components/Hero-Area/Hero.js
@@ -2,19 +2,9 @@ import "./Hero.css";
 import Slider from "../SliderSection/Slider";
 import React from "react";
 const Hero = (props) => {
-  const myInput = React.useRef(null);
-  const [counter, setCounter] = React.useState(0);
-
-  function abc() {     
-     // const input = document.querySelector('#one'); // myInput
-      alert(myInput.current.value)
-  }
-
   return (
     <div className="hero_area">
-      <Slider counter="props.isTodayFriday"/>
-      <input id="one" type="text" ref={myInput}/>
-      <button onClick={abc}></button>
+      <Slider/>
     </div>
   );
 };
